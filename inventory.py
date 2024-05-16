cf9cf493385193f5f7863423804e8b1f71603b8d
Faris Riza Maulana <168288082+FarisRiza@users.noreply.github.com>
2024-05-16 21:52:07 +0700

    Update inventory.py
    gg

--- inventory.py
+++ inventory.py
@@ -1,5 +1,5 @@
 import csv
-#apalahhh
+#apalahhhh
 def read_monster_names(filename):   #Membaca data nama monster berdasarkan id monster
     monster_names = {}
     with open(filename, 'r') as file:
